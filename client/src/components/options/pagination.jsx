5f799009adcffe3c064c295f0cfe80a1e013697a
Andres Frei <andresfrei@gmail.com>
2023-10-08 18:57:50 -0300

    fix error paginacion

--- client/src/components/options/pagination.jsx
+++ client/src/components/options/pagination.jsx
@@ -1,16 +1,23 @@
-import { KEY_PAGINATION_ITEMS } from '../../config/constants'
+import { KEY_PAGINATION_CURRENT_PAGE, KEY_PAGINATION_ITEMS } from '../../config/constants'
 import useKey from '../../hooks/useKey'
 import { SelectInput } from '../ui/inputs'
 
 export default function Pagination () {
   const [items, setItems] = useKey(KEY_PAGINATION_ITEMS)
+  const [, setCurrentPage] = useKey(KEY_PAGINATION_CURRENT_PAGE)
+
+  const handleChange = (e) => {
+    const { value } = e.target
+    setCurrentPage(1)
+    setItems(Number(value))
+  }
 
   return (
     <div className='flex align-items-center'>
         <label className='option-label'>Cards por página</label>
         <SelectInput width='150px'
           value={items}
-          onChange={(e) => setItems(Number(e.target.value)) }
+          onChange={handleChange}
         >
           <option value="9">9</option>
           <option value="10">10</option>
